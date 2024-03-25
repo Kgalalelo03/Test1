87a61df06812a739efebb92c1f41678b5c2da413
Kgalalelo03 <137161049+Kgalalelo03@users.noreply.github.com>
2024-03-25 17:55:28 +0200

    text-formatting uploaded

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int mincharecters = 6;
+            int maxcharecters = 12;
+
+            Console.Write("Enter the  number of charecters  (between " + mincharecters + " and " + maxcharecters + " characters): ");
+            int passwordLength = int.Parse(Console.ReadLine());
+
+            do
+            {
+               
+                if (passwordLength < mincharecters || passwordLength > maxcharecters)
+                {
+                    Console.WriteLine("Password length must be between " + mincharecters + " and " + maxcharecters + " characters.");
+                    Console.Write("Enter the desired password length (between " + mincharecters + " and " + maxcharecters + " characters): ");
+                    passwordLength = int.Parse(Console.ReadLine());
+
+                }
+            } while (passwordLength < mincharecters|| passwordLength > maxcharecters);
+
+            string allowedChars = "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ0123456789!@$?_-";
+            // Add or remove characters as desired
+
+            StringBuilder password = new StringBuilder();
+            Random random = new Random();
+
+            while (0 < passwordLength--)
+            {
+                password.Append(allowedChars[random.Next(allowedChars.Length)]);
+            }
+
+            Console.WriteLine("Random Password: " + password);
+        }
+    }
+}
